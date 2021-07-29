f1737c87efc88861862b62c87fc2168b9abdded9
jojo157 <carrigj@tcd.ie>
2021-07-29 19:41:33 +0000

    added host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-_33!af!wfvbbw=b59j&+rh8mtrnrgjk+7z(spq3hw#9f7o!9tn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['joanne-django-todolist.herokuapp.com']
 
 
 # Application definition
