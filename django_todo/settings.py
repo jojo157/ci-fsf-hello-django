93bef5de7b5111ef0c8620df68f3bc5194bf7ebd
jojo157 <carrigj@tcd.ie>
2021-07-30 18:54:33 +0000

    added environemtn variables

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get['SECRET_KEY']
+SECRET_KEY = os.environ.get['SECRET_KEY', 'django-insecure-_33!af!wfvbbw=b59j&+rh8mtrnrgjk+7z(spq3hw#9f7o!9tn']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
